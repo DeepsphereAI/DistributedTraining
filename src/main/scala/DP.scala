e39bac3a89824f56d0f31569635b709df0349656
vengi <DeepSphereAIInfo@gmail.com>
2021-02-21 20:03:59 -0800

    Added comments

--- src/main/scala/DP.scala
+++ src/main/scala/DP.scala
@@ -26,6 +26,9 @@ object DP {
       //.csv("hdfs://localhost:9820/test/DS.AI_Sales_Trans_Australia_csv.csv", "hdfs://localhost:9820/test/DS.AI_Sales_Trans_Canada_csv.csv") //5000+
       .csv("hdfs://localhost:9820/test/DS.AI_Sales_Trans_V3_big_csv.csv")
 
+    /*
+    Step #3 :  Set Partition count as needed
+    */
     val vAR_no_of_partitions =  5
 
     val vAR_partitioned_filePath = "C:/Users/vengi/ML/partitioned/"
@@ -37,13 +40,15 @@ object DP {
     vAR_sales_df_part.write.csv(vAR_partitioned_filePath)
 
     /*
-   Step #3 :  Pre-processing Training Data
-   */
+   Step #4 :  Pre-process Training Data
+    */
     val vAR_sales_df_speccols = vAR_sales_df_part
       .select("CustomerID", "ProductID", "TerritoryID", "Quantity", "OrderQty", "LineTotal")
 
-    //VectorAssembler is a transformer as it takes the input dataframe and returns the transformed dataframe
-    //with a new column which is vector representation of all the features.
+    /*
+    Step #5 : VectorAssembler is a transformer as it takes the input dataframe and returns the transformed dataframe
+              with a new column which is vector representation of all the features.
+    */
     val vAR_vectorassembler = new VectorAssembler()
       .setInputCols(Array("CustomerID", "ProductID", "TerritoryID", "Quantity", "OrderQty", "LineTotal"))
       .setOutputCol("features")
@@ -52,42 +57,50 @@ object DP {
     vAR_output.select("features", "LineTotal").show(false)
     val vAR_labelcols = vAR_output.select("features", "LineTotal")
 
-    //rename linetotal as label, now training data has two cols Features, Label only
+    /*
+    Step #6 :Rename linetotal as label, now training data has two cols Features, Label only
+    */
+
     val vAR_datatransformed = vAR_labelcols.withColumnRenamed("LineTotal", "label")
     val vAR_splitupdata = vAR_datatransformed.randomSplit(Array(0.80, 0.20))
     val vAR_training_data = vAR_splitupdata(0)
     val vAR_test_data = vAR_splitupdata(1)
 
     val vAR_lr = new LinearRegression()
-      .setMaxIter(10)
-      .setRegParam(0.3)
-      .setElasticNetParam(0.8)
-      .setFeaturesCol("features")
-      .setLabelCol("label")
+    .setMaxIter(10)
+    .setRegParam(0.3)
+    .setElasticNetParam(0.8)
+    .setFeaturesCol("features")
+    .setLabelCol("label")
+
     /*
-   Step #4 : Train Data
+    Step #7 : Train Data
     */
     val vAR_lrmodel = vAR_lr.fit(vAR_training_data)
 
     /*
-   Step #5 :  Predict on Test Data
-   */
+    Step #8 :  Predict on Test Data
+    */
     val vAR_predictions = vAR_lrmodel.transform(vAR_test_data)
     vAR_predictions.show()
 
     println(s"Coefficients: ${vAR_lrmodel.coefficients} Intercept: ${vAR_lrmodel.intercept}")
 
-    // Summarize the model over the training set and print out some metrics
+    /*
+    Step #9 :Summarize the model over the training set and print out some metrics
+    */
     val vAR_trainingSummary = vAR_lrmodel.summary
     println(s"numIterations: ${vAR_trainingSummary.totalIterations}")
     println(s"objectiveHistory: [${vAR_trainingSummary.objectiveHistory.mkString(",")}]")
     vAR_trainingSummary.residuals.show()
     println(s"RMSE: ${vAR_trainingSummary.rootMeanSquaredError}")
     println(s"r2: ${vAR_trainingSummary.r2}")
+    /*
+    Step #10 : Uncomment System.in.read command and comment vAR_spark.stop() to verify at Spark Jobs
+      System.in.read
+    */
+    vAR_spark.stop()
 
-    System.in.read
-    //spark.stop()
 
-
-  }
+}
 }
